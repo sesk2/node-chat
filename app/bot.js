6eb6c283e0228b3c152e752f782d84f3d31ac9aa
瀬良和優 <serakazuyoshi@seyoshikazuyuus-MacBook-Air.local>
2019-04-07 23:33:30 +0900

    2nd

--- app/bot.js
+++ app/bot.js
@@ -1,8 +1,40 @@
 const Bot = function () {};
 
+var word_list = [
+  {
+    'key': 'test',
+    'res': 'fuck yourself!'
+  },
+  {
+    'key': 'hello',
+    'res': 'shut the fuck up!',
+  },
+  {
+    'key': 'bye',
+    'res': 'dont go...'
+  }
+]
+
+
 Bot.response = function(msg) {
-  response = 'I understand you "' + msg + '".';
+  ///response = 'I understand you "' + msg + '".';
+  response = Bot.deal(msg);
   console.log(response);
+  return(response);
+};
+
+Bot.deal = function(msg) {
+  var check = 0;
+  word_list.forEach(function(word){
+    if(msg == word.key){
+      check++;
+      console.log(word.res);
+      response = word.res;
+    }
+  });
+  if(check == 0){
+    response = 'I dont understand you.';
+  }
   return(response);
 };
 
